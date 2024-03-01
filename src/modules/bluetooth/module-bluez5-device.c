704ec7004d102e45eacea6a1d9422caef504a8d3
Matti Lehtimäki <matti.lehtimaki@jolla.com>
2024-03-02 01:40:55 +0200

    Revert "bluetooth: Do not create a card profile for unsupported profiles"
    This reverts commit f7acc2ad7d77c32083f749446a5184ad3f482639.

--- src/modules/bluetooth/module-bluez5-device.c
+++ src/modules/bluetooth/module-bluez5-device.c
@@ -2302,11 +2302,6 @@ static int add_card(struct userdata *u) {
             continue;
         }
 
-        if (!pa_bluetooth_device_supports_profile(d, profile)) {
-            pa_log_debug("%s is not supported by the device or adapter", pa_bluetooth_profile_to_string(profile));
-            continue;
-        }
-
         cp = create_card_profile(u, profile, data.ports);
         pa_hashmap_put(data.profiles, cp->name, cp);
     }
